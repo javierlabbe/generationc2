89b3e78b26f23a1750525b231b3302ff69f76b01
javierlabbe <javier.labbe@uc.cl>
2022-11-03 21:26:30 -0300

    StarUML

---
+++ BaseDatos/Model/Direccion.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Direccion {
+
+    /**
+     * Default constructor
+     */
+    public Direccion() {
+    }
+
+    /**
+     * 
+     */
+    private string calle;
+
+    /**
+     * 
+     */
+    private string numero;
+
+    /**
+     * 
+     */
+    private string sector;
+
+    /**
+     * 
+     */
+    private string ciudad;
+
+    /**
+     * 
+     */
+    private string region;
+
+    /**
+     * 
+     */
+    public void Direccion() {
+        // TODO implement here
+    }
+
+}
